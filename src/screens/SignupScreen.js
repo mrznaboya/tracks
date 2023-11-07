44e0cb47c2fa6d92a0b8220b0212b3e81f94134d
mrznaboya <zuyax@yahoo.com>
2023-11-07 19:43:42 +0800

    Worked on SignupScreen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Text, Input, Button } from "@rneui/themed";
+
+import Spacer from "../components/Spacer";
 
 const SignupScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SignupScreen</Text>;
+  const navigation = useNavigation();
+  return (
+    <>
+      <Spacer>
+        <Text h3>Sign up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Input label="Password" />
+      <Button title="Sign Up" />
+    </>
+  );
 };
-
 const styles = StyleSheet.create({});
 
 export default SignupScreen;
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const TrackListScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackListScreen</Text>;
+  const navigation = useNavigation();
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+      <Button
+        title="Go to Track Detail"
+        onPress={() => navigation.navigate("TrackDetail")}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
