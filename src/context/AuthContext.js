f75b23895d1ee20eebfe9d771432247afe1e1915
mrznaboya <zuyax@yahoo.com>
2023-11-07 20:31:17 +0800

    Wire up AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,32 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return () => {
+    // Make api request to sign up with that email and password
+    // If we sign up, modify our state, and say that we are
+    // authenticated
+    // If signing up fails, we probably need to reflct an error message
+    // somewhere
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to signin
+    // Handle success by updating state
+    // Handle failure by showing error message(somehow)
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // Somehow sign out
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedIn: false }
 );
