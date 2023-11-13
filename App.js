5b413a396631dd561b6d81af09c82b97580c1ec3
mrznaboya <zuyax@yahoo.com>
2023-11-13 21:38:21 +0800

    Relocated files

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import TrackListScreen from "./src/screens/TrackListScreen";
 import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
 
 import { Provider as AuthProvider } from "./src/context/AuthContext";
-import { setNavigator } from "./navigationRef";
+import { setNavigator } from "./src/navigationRef";
 import { Provider as LocationProvider } from "./src/context/LocationContext";
 import { Provider as TrackProvider } from "./src/context/TrackContext";
 
--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -12,7 +12,8 @@ const locationReducer = (state, action) => {
       return { ...state, locations: [...state.locations, action.payload] };
     case "change_name":
       return { ...state, name: action.payload };
-
+    case "reset":
+      return { ...state, name: "", locations: [] };
     default:
       return state;
   }
@@ -50,6 +51,12 @@ const addLocation = (dispatch) => (location, recording) => {
   }
 };
 
+const reset = (dispatch) => () => {
+  dispatch({
+    type: "reset",
+  });
+};
+
 export const { Context, Provider } = createDataContext(
   locationReducer,
   {
@@ -57,6 +64,7 @@ export const { Context, Provider } = createDataContext(
     stopRecording,
     addLocation,
     changeName,
+    reset,
   },
   { name: " ", recording: false, locations: [], currentLocation: null }
 );
